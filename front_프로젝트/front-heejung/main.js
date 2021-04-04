e7ae9a436ef158e55ae481d7954a2fd865663e24
heejung-gjt <hee_9803@naver.com>
2021-04-04 16:46:51 +0900

    feat: upload html/css

--- front_프로젝트/front-heejung/main.js
+++ front_프로젝트/front-heejung/main.js
@@ -1,39 +1,36 @@
-const $submit = document.querySelector('.submit');
-const $box = document.querySelector('.box');
-const $boxContent = document.querySelector('.box-submit');
-let todos = [];
-const $close = document.querySelector('.close');
-const $textarea = document.querySelector('textarea');
-const $contentItem = document.querySelector('.content-items');
+const $mainItems = document.querySelector('.main-items');
+const $main = document.querySelector('.main');
+const $liContent = document.querySelector('.li-content');
+const $prc = document.querySelector('.prc')
+const $prcBtn = document.querySelector('.prc-btn');
 
-let now = new Date();
-$submit.onclick=()=>{
-    $box.style.display='block';
-}
-$close.onclick=()=>{
-    $box.style.display='none';
 
-}
-$boxContent.onclick=()=>{
-    if($textarea.value=='')return;
-    console.log($textarea.value)
-    $textarea.value='';
-    $box.style.display='none';
+const removeUl = () => {
+    $prc.scrollIntoView({
+        block:'start',
+        behavior:'smooth',
+    });
+    $mainItems.style.display='none';
+
 }
 
-// const render=()=>{
-//     $contentItem.innerHTML = todos.map(todo=>{
-//         return `<li class=${todo.id}><span><img src="" alt="">${todo.date}</span></li>`
-//     }).join();
-// }
-
-const getTodos = () =>{
-    todos=[
-        {id:1,content:'hi',date:now},
-        {id:2,content:'hiddd',date:now}
-    ]
-
-    // render();
+$mainItems.onclick = e => {
+    if(e.target.classList.contains('far')){
+        e.target.parentNode.style.display="none";
+    };
+    if(e.target.classList.contains('img')){
+        $prc.style.display='block';
+        removeUl();
+    }
 }
 
-getTodos();
+$prcBtn.onclick = () => {
+    $mainItems.style.display='flex';
+    window.scroll({
+        top:0,
+        behavior:'smooth'
+    })
+    setTimeout(function(){
+        $prc.style.display='none';
+    },1000)
+}
